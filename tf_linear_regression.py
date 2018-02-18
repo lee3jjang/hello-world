10be1689aea790d5f617fa6f32122f24edb81c58
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-02-18 17:11:30 +0900

    Update tf_linear_regression.py

--- tf_linear_regression.py
+++ tf_linear_regression.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 x_data = np.random.rand(100).astype(np.float32)
 y_data = 3*x_data + 2 
@@ -10,15 +12,33 @@ b = tf.Variable(0.2)
 y = a*x_data + b
 loss = tf.reduce_mean(tf.square(y-y_data))
 
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+optimizer = tf.train.GradientDescentOptimizer(0.1)
 train = optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
 train_data = []
 with tf.Session() as sess:
     sess.run(init)
-    for step in range(10000):
+    for step in range(100):
         evals = sess.run([train,a,b])[1:]
-        train_data.append([step, evals])
+        
         if step % 5 == 0:
+            train_data.append(evals)
             print(step, evals)
+            
+converter = plt.colors
+cr, cg, cb = (1.0,1.0,0.0)
+for f in train_data:
+    cb += 1.0 / len(train_data)
+    cg -= 1.0 / len(train_data)
+    if cb > 1.0: cb = 1.0
+    if cg < 0.0: cg = 0.0
+    [a, b] = f
+    f_y = np.vectorize(lambda x: a*x + b)(x_data)
+    line = plt.plot(x_data, f_y)
+    plt.setp(line, color=(cr,cg,cb))
+
+plt.plot(x_data, y_data,'ro')
+green_line = mpatches.Patch(color='red', label='Data Points')
+plt.legend(handles=[green_line])
+plt.show()
