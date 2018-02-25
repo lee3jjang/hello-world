93521374ec66a5484e4b4d48e4037d122ae2fba5
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-02-25 18:52:36 +0900

    Create name_export.py

---
+++ python/name_export.py
@@ -0,0 +1,8 @@
+print('this is export file')
+print(__name__)
+
+if __name__ == '__main__':
+    print('the name is main!!')
+
+if __name__ == 'main_export':
+    print('the name is this file name!')
