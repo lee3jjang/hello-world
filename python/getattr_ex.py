8829f4127958c36be55a5cbc7eb9bfd87663801b
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-03-03 22:44:00 +0900

    Create getattr_ex.py

---
+++ python/getattr_ex.py
@@ -0,0 +1,13 @@
+class sangjin():
+    def __init__(self):
+        self.name = "sangjin"
+        self.age = 29
+        self.height = 173
+        self.weight = 80
+    def crawl(self):
+        print("wal wal!")
+        
+
+
+x = sangjin()
+getattr(x,"crawl")()
