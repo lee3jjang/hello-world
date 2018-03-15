c856f93f9d89b6dce55788a6d8d03c9331f2741b
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-03-15 22:31:56 +0900

    Create client.py

---
+++ python/client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = 'localhost'
+PORT = 9009
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    msg = []
+    while msg!='q':
+        msg = input("메시지 입력:")
+        sock.sendall(msg.encode())
+        data = sock.recv(1024)
+        print("에코서버로부터 받은 데이터 [%s]" % data.decode())
