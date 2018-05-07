9097b8ec6a4971043611e6e1d0546f2d6587c8d3
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-05-07 19:25:17 +0900

    Delete 180507_crawling_autorun.bat

--- python/180507_crawling_autorun.bat
+++
@@ -1,3 +0,0 @@
-schtasks /create /sc minute /tn crawling /tr "C:\ProgramData\Anaconda3\python.exe C:\Users\noilkwon\dev\python\180507_Cetizen.py"
-schtasks /run /tn crawling
-#schtasks /delete crawling
