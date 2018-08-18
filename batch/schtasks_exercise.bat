83a1fcd70bb475f89e13495df41920872fde2890
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-08-19 05:12:40 +0900

    Add files via upload

---
+++ batch/schtasks_exercise.bat
@@ -0,0 +1,5 @@
+﻿schtasks /create /tn "내프로그램" /tr "C:\ProgramData\Anaconda3\python.exe C:\Users\Administrator\Downloads\test.py" /sc minute  /mo 5 /st 05:12 /et 05:30 /sd 2018/08/19
+schtasks /run /tn "내프로그램"
+schtasks /end /tn "내프로그램"
+schtasks /delete /f /tn "내프로그램"
+
