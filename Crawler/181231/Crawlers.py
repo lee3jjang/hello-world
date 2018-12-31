c98ed966000381ad5e62647a0b5c0cbb815d471f
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2018-12-31 13:27:48 +0900

    Add files via upload

---
+++ Crawler/181231/Crawlers.py
@@ -0,0 +1,26 @@
+"""
+Crawler list class
+
+When listing each websites or any other urls to get any webtoon information, list up Crawlers.py,
+You haver to initialize each Crawler with name and you have to choose which way to get data like HtmlCrawler or BaseCrawler.
+And then the class returns new webtoon list.
+"""
+
+__author__ = 'sj'
+
+from HtmlCrawler import HtmlCrawler
+from urllib.parse import urljoin
+
+class NaverCrawler(HtmlCrawler):
+    def __init__(self):
+        HtmlCrawler.__init__(self, 'http://comic.naver.com/webtoon/weekday.nhn')
+    
+    def get(self):
+        soup = HtmlCrawler.getBs(self)
+        data = soup.findAll('div',{'class':'thumb'})
+        
+        result = []
+        for obj in data:
+            if obj.find('em', {'class':'ico_updt'}) != None:
+                result.append(obj.img['title'] + " / " + urljoin(self.url, obj.a['href']))
+        return result
