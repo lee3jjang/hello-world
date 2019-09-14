44eec09ca2794e30099ec1e27104a414ad97afbc
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2019-09-14 23:32:19 +0900

    Update dart_gongsi.py

--- Crawler/dart_gongsi/dart_gongsi.py
+++ Crawler/dart_gongsi/dart_gongsi.py
@@ -23,9 +23,10 @@ def send_message(text):
             https://hexoul.blogspot.com/2017/11/telegram-sendmessage-api.html
     '''
     access_token = '904469782:AAGKkT977saP5wOVcAl18lALBYbqhKx6V28'
+    chat_id = '904981301'
     # url = 'https://api.telegram.org/bot{}/getUpdates'.format(access_token)
     url = 'https://api.telegram.org/bot{}/sendMessage'.format(access_token)
-    res = requests.post(url, data={'chat_id': '904981301', 'text': text})
+    res = requests.post(url, data={'chat_id': chat_id, 'text': text})
     if res.status_code == 200:
         now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         print('({}) {}: 성공적으로 전송되었습니다.'.format(now, __name__ + '.' + inspect.stack()[0].function))
