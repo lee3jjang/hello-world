1c03de6a78c0a440b398622ebe9c50d4974e8711
lee3jjang <36585973+lee3jjang@users.noreply.github.com>
2020-10-15 17:02:06 +0900

    Add files via upload

--- python/mymenu.py
+++ python/mymenu.py
@@ -6,13 +6,13 @@ from kivy.clock import Clock
 from kivymd.uix.label import MDLabel
 from kivy.animation import Animation
 from kivy.properties import StringProperty, NumericProperty
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.button import MDRaisedButton, MDFlatButton
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.tab import MDTabsBase
 from kivymd.font_definitions import theme_font_styles
-print(theme_font_styles)
+from kivy.uix.screenmanager import FadeTransition, NoTransition
 Window.size = (144*3, 256*3)
 
 KV = '''
@@ -33,35 +33,93 @@ BoxLayout:
         id: timer
         text: '{:02d}:{:02d}.{:02d}'.format(int(int(self.time)/60), int(self.time)%60, int(100*(self.time-int(self.time))))
         size_hint: None, None
-        font_style: 'H3'
-        size: 144*3, 700
+        font_style: 'H2'
+        size: 144*3, 800
         halign: 'center'
         valign: 'middle'
 
-    StopwatchButton:
-        
-        text: 'Start'
-        size_hint: None, None
-        size: 144*3, 50
-        on_release:
-            self.start(*args)
+    ScreenManager:
+        id: sm_buttons
+        Screen:
+            name: 'start_screen'
+            StopwatchButton:
+                text: 'Start'
+                size_hint: 1, 0.1
+                font_size: "18sp"
+                pos_hint: {'center_x': 0.5}
+                on_release:
+                    self.start(*args)
 
-        canvas:
-            Color: 0, 0, 0
-            Rectangle:
-                size: self.size
+
+        Screen:
+            name: 'stop_screen'
+            BoxLayout:
+                orientation: 'horizontal'
+                StopwatchButton:
+                    text: 'Stop'
+                    text_color: 1, 0, 0, 1
+                    started: True
+                    size_hint: 1, 0.1
+                    font_size: "18sp"
+                    pos_hint: {'center_x': 0.5}
+                    on_release:
+                        self.start(*args)
+
+                StopwatchButton:
+                    text: 'Record'
+                    size_hint: 1, 0.1
+                    font_size: "18sp"
+                    pos_hint: {'center_x': 0.5}
+                    on_release:
+                        pass
+
+        Screen:
+            name: 'continue_screen'
+            BoxLayout:
+                orientation: 'horizontal'
+                StopwatchButton:
+                    text: 'Continue'
+                    started: False
+                    size_hint: 1, 0.1
+                    font_size: "18sp"
+                    pos_hint: {'center_x': 0.5}
+                    on_release:
+                        self.start(*args)
+
+                StopwatchButton:
+                    text: 'Initialize'
+                    size_hint: 1, 0.1
+                    font_size: "18sp"
+                    pos_hint: {'center_x': 0.5}
+                    on_release:
+                        self.initialize(*args)
+        
 '''
 
-class StopwatchButton(MDRaisedButton):
+class StopwatchButton(MDFlatButton):
     started = False
     def start(self, instance_button):
         if self.started == False:
-            instance_button.parent.ids.timer.start()
-            self.text = 'Stop'
-            self.started = True
+            if instance_button.text == 'Continue':
+                instance_button.parent.parent.parent.parent.ids.timer.start()
+                instance_button.parent.parent.parent.parent.ids.sm_buttons.current = 'stop_screen'
+            elif instance_button.text == 'Start':
+                instance_button.parent.parent.parent.ids.timer.start()
+                instance_button.parent.parent.parent.ids.sm_buttons.transition = NoTransition()
+                instance_button.parent.parent.parent.ids.sm_buttons.current = 'stop_screen'
+            else:
+                raise Exception()
         else:
-            Animation.stop_all(instance_button.parent.ids.timer)
-            self.started = False
+            Animation.stop_all(instance_button.parent.parent.parent.parent.ids.timer)
+            instance_button.parent.parent.parent.parent.ids.sm_buttons.current = 'continue_screen'
+
+    def initialize(self, instance_button):
+        instance_button.parent.parent.parent.parent.ids.timer.time = 0
+        instance_button.parent.parent.parent.parent.ids.sm_buttons.current = 'start_screen'
+
+    def record(self, instance_button):
+        pass
+
 
 
 class StopwatchTab(FloatLayout, MDTabsBase):
@@ -69,17 +127,19 @@ class StopwatchTab(FloatLayout, MDTabsBase):
 
 
 class Stopwatch(MDLabel):
-    time = NumericProperty()
+    time = NumericProperty(10)
 
     def start(self):
         self.anim = Animation(time=1e9, duration=1e9)
-        # def callback(animation, widget):
-        #     widget.text = 'Finished'
-        # self.anim.bind(on_complete = callback)
         self.anim.start(self)
 
-
 class Test(MDApp):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.theme_cls.primary_palette = 'Red'
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_hue = "300"
+
     def build(self):
         return Builder.load_string(KV)
 
