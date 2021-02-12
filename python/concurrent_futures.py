f2a4f8f2ae4b83f9b3c84faf4ac03ffd6b17591a
Sang-Jin <36585973+lee3jjang@users.noreply.github.com>
2021-02-12 11:41:36 +0900

    Add files via upload

---
+++ python/concurrent_futures.py
@@ -0,0 +1,27 @@
+import concurrent.futures
+import urllib.request
+
+URLS = [
+    'http://www.foxnews.com/',
+    'http://www.cnn.com/',
+    'http://europe.wsj.com/',
+    'http://www.bbc.co.uk/',
+    'http://some-made-up-domain.com/',
+]
+
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    future_to_url = {
+        executor.submit(load_url, url, 60): url for url in URLS
+    }
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print(f'{url} generated an exception: {exc}')
+        else:
+            print(f'{url} page is {len(data)} bytes')
